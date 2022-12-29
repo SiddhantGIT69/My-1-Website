acc222eea8510e445bbf349c6b5a6e3d2dd43ff0
Siddhant <98139699+SiddhantGIT69@users.noreply.github.com>
2022-12-29 11:19:06 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+
+    function topFunction() {
+        document.documentElement.scrollTop = 0;
+    }
+
+    let nav_offset_top = $('.header-area').height() + 50;
+
+    function navbarFixed() {
+        if ($('.header-area').length) {
+            $(window).scroll(function () {
+                let scroll = $(window).scrollTop();
+                if (scroll >= nav_offset_top) {
+                    $('.header-area .main-menu').addClass('navbar_fixed');
+                } else {
+                    $('.header-area .main-menu').removeClass('navbar_fixed');
+                }
+            })
+        }
+
+    }
+
+    navbarFixed();
+})
